7ddf61dda10b6e6cd15e98e48f17b33353a09bb5
albardespinozaaldaco <albardespinoza@gmail.com>
2018-06-15 13:32:05 -0700

    If

--- main.cpp
+++ main.cpp
@@ -5,29 +5,43 @@
 using namespace std;
 
 int main() {
-    char gradeLetter[2];
-    float grade;
+    string gradeLetter;
+    double grade = 10;
 
-    cout << "Enter a letter grade between A-F: ";
+    cout << "Enter a letter grade: ";
     cin >> gradeLetter;
 
-    if (gradeLetter == "A") {
-        grade = 4.0;
-    } else if (gradeLetter == "A-") {
-        grade = 3.7;
-    } else if (gradeLetter == "B+") {
-        grade = 3.3;
-    } else if (gradeLetter == "B") {
-        grade = 3.0;
-    } else if (gradeLetter == "B-") {
-        grade = 2.7;
-    } else if (gradeLetter == "C+") {
-        grade = 2.3;
-    } else if (gradeLetter == "C") {
-        grade = 2.0;
-    } else if (gradeLetter == "C-") {
-        grade = 1.7;
+    if (gradeLetter[0] == 'A' || gradeLetter[0] == 'B' || gradeLetter[0] == 'C' || gradeLetter[0] == 'D' ||
+        gradeLetter[0] == 'F') {
+        if (gradeLetter == "A") {
+            grade = 4;
+        } else if (gradeLetter == "A-") {
+            grade = 3.7;
+        } else if (gradeLetter == "B+") {
+            grade = 3.3;
+        } else if (gradeLetter == "B") {
+            grade = 3.0;
+        } else if (gradeLetter == "B-") {
+            grade = 2.7;
+        } else if (gradeLetter == "C+") {
+            grade = 2.3;
+        } else if (gradeLetter == "C") {
+            grade = 2.0;
+        } else if (gradeLetter == "C-") {
+            grade = 1.7;
+        } else if (gradeLetter == "D+") {
+            grade = 1.3;
+        } else if (gradeLetter == "D") {
+            grade = 1.0;
+        } else if (gradeLetter == "D-") {
+            grade = 0.7;
+        } else if (gradeLetter == "F") {
+            grade = 0.0;
+        }
+        cout << setprecision(1) << fixed;
+        cout << "The numeric value is " << grade;
+    } else {
+        cout << "Please enter a letter grade between A-F.";
     }
-
 
 }
