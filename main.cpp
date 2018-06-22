8b3854aebcecdd691277746985c7e5dea0918334
albardespinozaaldaco <albardespinoza@gmail.com>
2018-06-22 12:07:09 -0700

    Latest version.

--- main.cpp
+++ main.cpp
@@ -5,43 +5,31 @@
 using namespace std;
 
 int main() {
-    string gradeLetter;
-    double grade;
 
-    cout << "Enter a letter grade: ";
-    cin >> gradeLetter;
+    float stockPrice, stockTotal = 0;
+    int stockCount = 0, days = 1;
 
-    if (gradeLetter[0] >= 'A' && gradeLetter[0] <= 'F') {
-        if (gradeLetter == "A") {
-            grade = 4;
-        } else if (gradeLetter == "A-") {
-            grade = 3.7;
-        } else if (gradeLetter == "B+") {
-            grade = 3.3;
-        } else if (gradeLetter == "B") {
-            grade = 3.0;
-        } else if (gradeLetter == "B-") {
-            grade = 2.7;
-        } else if (gradeLetter == "C+") {
-            grade = 2.3;
-        } else if (gradeLetter == "C") {
-            grade = 2.0;
-        } else if (gradeLetter == "C-") {
-            grade = 1.7;
-        } else if (gradeLetter == "D+") {
-            grade = 1.3;
-        } else if (gradeLetter == "D") {
-            grade = 1.0;
-        } else if (gradeLetter == "D-") {
-            grade = 0.7;
-        } else if (gradeLetter == "F") {
-            grade = 0.0;
+    cout << "In the following statement please enter the closing value of a stock\nin a certain number of days. "
+            "When done enter '-1'.";
+
+    do {
+        cout << "Please enter the closing value for day 1: ";
+        cin >> stockPrice;
+        if (stockPrice == -1) {
+            break;
+        } else {
+            stockCount++;
+            days++;
+            stockTotal += stockPrice;
         }
-        cout << setprecision(1) << fixed;
-        cout << "The numeric value is " << grade;
+    } while (stockPrice != -1);
+
+    if (stockPrice == -1 && days == 1) {
+        cout << "No stock was entered.";
     } else {
-        cout << "Please enter a letter grade between A-F.";
+        stockTotal /= stockCount;
+        cout << setprecision(4) << fixed;
+        cout << "The average closing price is: " << stockTotal;
     }
-    char x = 'a';
 
 }
