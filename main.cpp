411e7216e138f266c68df3610d8abe477afa7bdc
albardespinozaaldaco <albardespinoza@gmail.com>
2018-06-29 11:39:25 -0700

    Latest version.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,59 @@
 #include <iostream>
 #include <iomanip>
 
+double bMembership(double);
+
+double sMembership(double);
+
+double pMembership(double);
+
 using namespace std;
 
 int main() {
 
-    float stockPrice, stockTotal = 0;
-    int stockCount = 0, days = 1;
+    char x;
+    double y;
+    cout << "Wholesale Membership Rewards Calculator\n\n";
+    cout << "1. Basic Membership\n2. Standard Membership\n3. Premium Membership\n4. Quit\nEnter your choice: ";
+    cin >> x;
+    cout << "Enter your monthly purchase total: $";
+    cin >> y;
 
-    cout << "In the following statement, please enter the closing value of a stock\nin a certain number of days. "
-            "When done enter '-1'." << endl;
-
-    do {
-        cout << "Please enter the closing value for day " << days << ": ";
-        cin >> stockPrice;
-        if (stockPrice != -1) {
-            stockCount++;
-            days++;
-            stockTotal += stockPrice;
+    while (x != '4') {
+        double z;
+        if (x == '1') {
+            z = bMembership(y);
         }
-    } while (stockPrice != -1);
-
-    if (stockPrice == -1 && days == 1) {
-        cout << "No stock was entered!";
-    } else {
-        stockTotal /= stockCount;
-        cout << setprecision(4) << fixed;
-        cout << "The average closing price is: " << stockTotal;
     }
 
+}
+
+double bMembership(double x) {
+    double y;
+    if (x <= 100) {
+        y = 0.1 * x;
+    } else {
+        y = 0.15 * x;
+    }
+    return y;
+}
+
+double sMembership(double x) {
+    double y;
+    if (x <= 100) {
+        y = 0.15 * x;
+    } else {
+        y = 0.2 * x;
+    }
+    return y;
+}
+
+double pMembership(double x) {
+    double y;
+    if (x <= 100) {
+        y = 0.2 * x;
+    } else {
+        y = 0.3 * x;
+    }
+    return y;
 }
