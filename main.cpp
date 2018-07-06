9bcce3df80222674d02c374d3fab6e74f772f0e2
albardespinozaaldaco <albardespinoza@gmail.com>
2018-07-06 10:00:03 -0700

    Latest version.

--- main.cpp
+++ main.cpp
@@ -1,80 +1,41 @@
 #include <iostream>
 #include <iomanip>
 
-double bMembership(double);
-
-double sMembership(double);
-
-double pMembership(double);
-
 using namespace std;
+
+void displayArray(int [], int);
+
+double getAverage(int [], int);
 
 int main() {
 
-    int x;
-    double y;
-    cout << "Wholesale Membership Rewards Calculator\n\n";
-    cout << "1. Basic Membership\n2. Standard Membership\n3. Premium Membership\n4. Quit\nEnter your choice: ";
-    cin >> x;
-    while (x < 1 || x > 4) {
-        cout << "Please enter a valid value: ";
-        cin >> x;
-    }
-    while (x != 4) {
-        cout << "Enter your monthly purchase total: $";
-        cin >> y;
-        while (y < 0) {
-            cout << "Please enter a non=negative value: ";
-            cin >> y;
-        }
-        double z;
-        switch (x) {
-            case 1:
-                z = bMembership(y);
-                break;
-            case 2:
-                z = sMembership(y);
-                break;
-            case 3:
-                z = pMembership(y);
-                break;
-        }
-        cout << setprecision(0) << fixed;
-        cout << "\nThe rewards balance is $" << z << ".00\n\n";
+    int arr[5];
 
-        cout << "Wholesale Membership Rewards Calculator\n\n";
-        cout << "1. Basic Membership\n2. Standard Membership\n3. Premium Membership\n4. Quit\nEnter your choice: ";
-        cin >> x;
+    cout << "Please enter the scores of the 5 days." << endl;
+
+    for (int x = 0; x < 5; x++) {
+        cout << "Enter the score for day " << x + 1 << ": ";
+        cin >> arr[x];
+    }
+
+    cout << "The points of each day are: " << endl;
+    displayArray(arr, 5);
+    cout << "The average points of the 5 days are: " << endl;
+    cout << setprecision(2) << fixed;
+    cout << getAverage(arr, 5);
+
+}
+
+void displayArray(int points[], int numElements) {
+    for (int x = 0; x < numElements; x++) {
+        cout << "Score for day " << x + 1 << ": " << points[x] << endl;
     }
 }
 
-
-double bMembership(double x) {
-    double y;
-    if (x <= 100) {
-        y = 0.1 * x;
-    } else {
-        y = 0.15 * x;
+double getAverage(int points[], int numElements) {
+    double total;
+    for (int x = 0; x < numElements; x++) {
+        total += points[x];
     }
-    return y;
-}
-
-double sMembership(double x) {
-    double y;
-    if (x <= 100) {
-        y = 0.15 * x;
-    } else {
-        y = 0.2 * x;
-    }
-    return y;
-}
-
-double pMembership(double x) {
-    double y;
-    if (x <= 100) {
-        y = 0.2 * x;
-    } else {
-        y = 0.3 * x;
-    }
-    return y;
+    return total / numElements;
 }
