05f3a102c36087171c14fb31dd1ed457250370b9
albardespinozaaldaco <albardespinoza@gmail.com>
2018-07-27 21:56:53 -0700

    Latest version.

--- main.cpp
+++ main.cpp
@@ -8,4 +8,20 @@ int main() {
     Person one("Steve", "34231 Grover Street", 34, "619-567-3462");
     Person two("Michael", "45349 Marshall Avenue", 29, "619-543-45678");
     Person three("Susan", "97528 Rochester Boulevard", 38, "619-678-3421");
+
+    cout << "Name: " << one.getName() << endl;
+    cout << "Address: " << one.getAddress() << endl;
+    cout << "Age: " << one.getAge() << endl;
+    cout << "Phone Number: " << one.getPhoneNumber() << endl;
+
+    cout << "\nName: " << two.getName() << endl;
+    cout << "Address: " << two.getAddress() << endl;
+    cout << "Age: " << two.getAge() << endl;
+    cout << "Phone Number: " << two.getPhoneNumber() << endl;
+
+    cout << "\nName: " << three.getName() << endl;
+    cout << "Address: " << three.getAddress() << endl;
+    cout << "Age: " << three.getAge() << endl;
+    cout << "Phone Number: " << three.getPhoneNumber() << endl;
+
 }
